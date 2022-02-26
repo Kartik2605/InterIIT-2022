918a98d5a7973c6d52aafe0141f35b98d8787040
Kartik Ruikar'clear <na20b053@smail.iitm.ac.in>
2022-02-26 22:11:20 +0530

    comitted the login.py file

---
+++ login.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from bs4 import BeautifulSoup
+
+username = "na20b053"
+password = "Fa8A+ZBL%M5"
+
+url = "https://www.iitm.ac.in/viewgrades/"
+
+s=Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service = s)
+
+driver.get(url)
+
+driver.find_element_by_name("rollno").send_keys(username)
+driver.find_element_by_name("pwd").send_keys(password)
+driver.find_element_by_css_selector("input[type=\"submit\" i]").click()
+
+print("LoggedIn Successfully!!")
+sleep(1)
+
+driver.get("https://www.iitm.ac.in/viewgrades/studentauth/studpass.php")
+
+#print(driver.find_element_by_xpath("/html/body/center/center/table[1]/tbody/tr[39]/td[2]/b/font").text)
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup('FRAME')
+for tag in tags:
+    print(tag.get('SRC', None))
+
+input("Press enter to end the program and close the site....")
